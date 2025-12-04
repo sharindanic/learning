62bbed24b1fb932204fb6c1ba468dc6ae2657844
Nicol fernando <166232317+sharindanic@users.noreply.github.com>
2025-12-04 16:29:33 +0530

    Create Marks.java with basic structure
    Initial implementation of Marks class with user input.

---
+++ week01/Marks.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Marks{
+  public static void main(Strig[]args){
+    System.out.println("enter the marks:");
+    Scanner sc=new Scanner;
+    String[]marks=new String[6];
+  }
+}
+
+
+
+
+
+//unifinished 
