b04a38c00596688cd98c1c8487f03c0ad616fe67
Nicol fernando <166232317+sharindanic@users.noreply.github.com>
2025-12-04 16:14:48 +0530

    Create ArrayNew.java with car array example
    Initialized an array of cars and printed one element.

---
+++ Arrays_broCode/ArrayNew.java
@@ -0,0 +1,17 @@
+public class ArrayNew{
+  public static void main(String []args){
+    String[]cars=new String[5];
+    cars[0]="Toyota";
+    cars[1]="Toyota";
+    cars[2]="Toyota";
+    cars[3]="Toyota";
+    cars[4]="Toyota";
+    cars[5]="Toyota";
+
+    System.out.println(cars[3]);
+    
+  }
+}
+
+//what ever car u want it will selected from arrays 
+//BroCode 
