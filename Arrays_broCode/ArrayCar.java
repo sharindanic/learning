9e577db6810790ba19ced05208ad6fe5f924fb42
Nicol fernando <166232317+sharindanic@users.noreply.github.com>
2025-12-04 15:51:16 +0530

    Add new ArrayCar.java file

---
+++ Arrays_broCode/ArrayCar.java
@@ -0,0 +1,7 @@
+public class ArrayCar{
+    public static void main(String[]args){
+        String[]cars={"Toyota","Nissan","Mistishubishi"};
+        cars[0]="Mustang";
+        System.out.println(cars[0]);
+    }
+}
