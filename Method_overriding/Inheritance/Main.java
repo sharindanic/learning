addc2fe40387f81158e975644fe00103226d1741
Nicol fernando <166232317+sharindanic@users.noreply.github.com>
2025-12-06 16:52:30 +0530

    Add main method to initialize EV instance

--- Method_overriding/Inheritance/Main.java
+++ Method_overriding/Inheritance/Main.java
@@ -1,1 +1,10 @@
-
+public class Main{
+  public static void main(String[]args){
+    EV mytesla = new EV();
+    mytesla.start();
+    mytesla.engine();
+    mytesla.petrol();
+    mytesla.sound();
+    
+  }
+}
