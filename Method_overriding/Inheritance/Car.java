3c292a07f6e4a9f1123274223ef978e1342bdd63
Nicol fernando <166232317+sharindanic@users.noreply.github.com>
2025-12-06 10:59:27 +0530

    Update Car.java

--- Method_overriding/Inheritance/Car.java
+++ Method_overriding/Inheritance/Car.java
@@ -1,1 +1,14 @@
-
+public class Car{
+  void start(){
+    System.out.println("Car is starting.....");
+  }
+  void engine(){
+    System.out.println("Engine getting worm");
+  }
+  void petrol(){
+    System.out.println("Petrol pump to engine");
+  }
+  void sound(){
+    System.out.println("Engine start making sound");
+  }
+}
