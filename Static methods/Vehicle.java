979617c9ff6667c5ecb80d2a251c54c13a06a27c
Nicol fernando <166232317+sharindanic@users.noreply.github.com>
2025-12-07 01:53:42 +0530

    Create Vehicle.java

---
+++ Static methods/Vehicle.java
@@ -0,0 +1,12 @@
+class Vehicle{
+  String name;
+  int price;
+  static String type;
+
+  public void displaying(){
+    System.out.println(name+":"+ price+":"+ type);
+  }
+  static void statdisplaying(){
+    System.out.println(name+":"+ price+":"+ type);
+  }
+}
