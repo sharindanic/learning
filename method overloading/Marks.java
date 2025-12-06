d51181f2a28810d21f9b4bc7d74d90a4d019aeb0
Nicol fernando <166232317+sharindanic@users.noreply.github.com>
2025-12-06 17:47:43 +0530

    Implement method overloading in Marks class

---
+++ method overloading/Marks.java
@@ -0,0 +1,11 @@
+class Marks{
+  public int add(int n1,int n2,int n3){
+    return n1+n2+n3;
+  }
+  public double add(int n1,double n2, int n3){
+    return n1+n2+n3;
+  }
+  public int add(int n1+n2){
+    return n1+n2;
+  }
+}
