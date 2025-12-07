a9b963f964366531c7cb4b274ae1e62607375176
Nicol fernando <166232317+sharindanic@users.noreply.github.com>
2025-12-07 21:48:54 +0530

    Create Main.java

---
+++ Encapsulation/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String []args){
+        Vehicle obj=new Vehicle();
+        obj.setname("toyota camry");
+        obj.setYear(2003);
+    
+        System.out.println(obj.getname() + ":" +obj.getYear());
+        
+    }
+    
+}
