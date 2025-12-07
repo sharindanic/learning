87f8b683a82fb85e8f15520781cec2755eadae70
Nicol fernando <166232317+sharindanic@users.noreply.github.com>
2025-12-07 22:54:49 +0530

    Update Main.java

--- Encapsulation/Main.java
+++ Encapsulation/Main.java
@@ -1,11 +1,9 @@
 public class Main{
     public static void main(String []args){
         Vehicle obj=new Vehicle();
-        obj.setname("toyota camry");
-        obj.setYear(2003);
+        obj.setmanifacture_date(2007);
+        obj.setname_of_the_car("Ferrari");
     
-        System.out.println(obj.getname() + ":" +obj.getYear());
+        System.out.println(obj.getname_of_the_car()+ ":"+ obj.getname_of_the_car());
         
-    }
-    
-}
+    }}
