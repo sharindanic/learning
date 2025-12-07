5753bb5f188e802465ed575a5f8e90b95b84ff50
Nicol fernando <166232317+sharindanic@users.noreply.github.com>
2025-12-07 21:48:30 +0530

    Create Vehicle.java

---
+++ Encapsulation/Vehicle.java
@@ -0,0 +1,17 @@
+public class Vehicle{
+    private String name;
+    private int Year;
+    
+    public String getname(){
+        return name;
+    }
+    public void setname(String car_name){
+        name=car_name;
+    }
+    public int getYear(){
+        return Year;
+    }
+    public void setYear(int manifacture_date){
+        Year=manifacture_date;
+    }
+}
