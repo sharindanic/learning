e3507e65d26e05ee14e156579ccf53cf540c64a4
Nicol fernando <166232317+sharindanic@users.noreply.github.com>
2025-12-08 23:37:25 +0530

    Create Main.java

---
+++ Inheritance/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String []args){
+        Adv obj1=new Adv();
+        int r1=obj1.add(3,4);
+        int r2=obj1.sub(10,3);
+        int r3=obj1.div(10,2);
+        int r4=obj1.multi(2,4);
+
+        System.out.println(r1+":"+r2+":"+r3+":"+r4);
+    }
+}
