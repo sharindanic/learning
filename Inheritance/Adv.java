081b3e9c184359fd66aa60a752ca094d68617f7a
Nicol fernando <166232317+sharindanic@users.noreply.github.com>
2025-12-08 23:38:20 +0530

    Create Adv.java

---
+++ Inheritance/Adv.java
@@ -0,0 +1,12 @@
+//advanced keyboard
+
+class Adv extends Cal{
+    public int div(int a,int b)
+    {
+        return a/b;
+    }
+    public int multi(int a,int b)
+    {
+        return a*b;
+    }
+}
