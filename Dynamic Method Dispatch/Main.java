1044d1e75cbe9f4c6af222bd565c72d6b2b89949
Nicol fernando <166232317+sharindanic@users.noreply.github.com>
2025-12-09 09:45:02 +0530

    Create Main.java

---
+++ Dynamic Method Dispatch/Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String []args){
+        Cal obj=new Cal();
+        obj.add();
+
+        obj=new Adv();
+        obj.add();
+
+        
+
+    }
+}
