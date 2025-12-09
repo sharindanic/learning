143e3260e0c5247da3580c73dae3e988e06b6538
Nicol fernando <166232317+sharindanic@users.noreply.github.com>
2025-12-10 00:54:38 +0530

    Create Main.java

---
+++ TryCatch/Main.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String []args){
+        int j=0;
+        int i=0;
+
+        try{
+            j=12/1;
+        }catch(Exception e){
+            System.out.println("This will occur run time error:");
+        }
+        System.out.println(j);
+        
+    }
+}
