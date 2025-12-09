ed8bc2680496520c13646b54ac6f51e464f20331
Nicol fernando <166232317+sharindanic@users.noreply.github.com>
2025-12-09 14:43:38 +0530

    Create Main.java

---
+++ Interface/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String []args){
+        Car obj=new B();
+        obj.manu();
+        obj.sub();
+
+        System.out.println(Car.model);
+    }
+}
