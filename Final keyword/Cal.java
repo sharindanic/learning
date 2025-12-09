dce179f9487b5fc4c2fa13f62a1b63a3cf86d67c
Nicol fernando <166232317+sharindanic@users.noreply.github.com>
2025-12-09 10:52:02 +0530

    Update Cal.java

--- Final keyword/Cal.java
+++ Final keyword/Cal.java
@@ -1,8 +1,13 @@
-class Cal{
-    public void add(int a,int b){
+final class Cal{ //to stop the inheritence 
+    public final void add(int a,int b){ // added final to stop the method overriding 
         System.out.println(a+b);
     }
-    public void sub(int a,int b){
+    public final void sub(int a,int b){ // ""
         System.out.println(a-b);
     }
 }
+
+
+//we use final in methods to stop the method overiding 
+// we use in varibale to make it  const 
+// can use in class to stop inheritence 
