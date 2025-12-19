d7adfe85965d98a937be867349279fb87928ff22
Nicol fernando <166232317+sharindanic@users.noreply.github.com>
2025-12-19 21:07:39 +0530

    Add Alien class for Spring Boot project

---
+++ springboot/Alien.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Alien {
+    public void build(){
+        System.out.println("Working on my first SpringBoot Project");
+    }
+}
