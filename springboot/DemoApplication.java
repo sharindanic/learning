97a88f2f08396ed678505a9bde58cbba3548df83
Nicol fernando <166232317+sharindanic@users.noreply.github.com>
2025-12-20 00:00:06 +0530

    Fix package declaration comment syntax

--- springboot/DemoApplication.java
+++ springboot/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+*/package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,3 +14,4 @@ public class DemoApplication {
     }
 
 }
+/*
