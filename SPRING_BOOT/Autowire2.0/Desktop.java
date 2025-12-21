282555e78828216cd807faa5ecc3f8e4316e7218
Nicol fernando <166232317+sharindanic@users.noreply.github.com>
2025-12-21 23:21:56 +0530

    Create Desktop.java

---
+++ SPRING_BOOT/Autowire2.0/Desktop.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Desktop implements Computer {
+    public void compile(){
+        System.out.println("Compiling with 404 Bugs but faster");
+    }
+}
