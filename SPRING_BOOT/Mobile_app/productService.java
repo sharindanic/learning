e1873c93825efcf5147a7396ade077708f119b27
Nicol fernando <166232317+sharindanic@users.noreply.github.com>
2026-01-02 23:54:34 +0530

    Update productService.java

--- SPRING_BOOT/Mobile_app/productService.java
+++ SPRING_BOOT/Mobile_app/productService.java
@@ -1,1 +1,56 @@
+package com.telusko.simplewebappp.service;
+import com.telusko.simplewebappp.model.Product;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class ProductService {
+    List<Product> products = new ArrayList<>(Arrays.asList
+            (new Product(101, "Iphone", 5000)
+                    , new Product(102, "Samsung", 3000)
+                    , new Product(103, "Pixel", 6000)));
+
+    public List<Product> getProduct() {
+        return products;
+    }
+
+    public Product getProductById(int prodId) {
+        return products.stream()
+                .filter(p -> p.getProdId() == prodId)
+                .findFirst()
+                .orElse(null);
+
+    }
+
+    public void addProduct(Product prod) {
+        products.add(prod);
+    }
+
+    public void updateProduct(Product prod) {
+        int index = 0;
+
+        for (int i = 0; i < products.size(); i++)
+            if (products.get(i).getProdId() == prod.getProdId())
+                index = i;
+
+        products.set(index, prod);
+    }
+
+
+    public void deleteProduct(int prodId) {
+        int index = -1;
+
+        for (int i = 0; i < products.size(); i++) {
+            if (products.get(i).getProdId() == prodId) {
+                index = i;
+                break;
+            }
+        }
+        if (index != -1) {
+            products.remove(index);
+        }
+    }
+}
